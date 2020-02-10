0f9fddc6c2d3725deed216d381d3c47f81c5ce96
Peter Rosemann <dkdndes@users.noreply.github.com>
2020-02-10 20:32:25 +0100

    Github actions test undone

--- obp_oauth/tests.py
+++ obp_oauth/tests.py
@@ -6,4 +6,4 @@ class ProjectTests(TestCase):
     def test_homepage(self):
         response = self.client.get('/')
         # for the test of Github Actions
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, 200)
