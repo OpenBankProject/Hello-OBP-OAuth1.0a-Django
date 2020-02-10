d683a11940dbbf49b72b210814ffa1d3c0633d41
Peter (dkdndes) <dkdndes@gmail.com>
2020-02-10 19:39:46 +0100

    added initial test stub

--- obp_oauth/tests.py
+++ obp_oauth/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ProjectTests(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
