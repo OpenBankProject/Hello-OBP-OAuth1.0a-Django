2e41a5a6d9b1e9619cb51ca9fea00066db768c28
Peter (dkdndes) <dkdndes@gmail.com>
2020-02-10 20:28:23 +0100

    corrected url to /bank/

--- obp_oauth/views.py
+++ obp_oauth/views.py
@@ -69,7 +69,7 @@ class AuthorizationView(TemplateView):
 
 
 class BankView(TemplateView):
-    template_name = "obp_oauth/authorization.html"
+    template_name = "obp_oauth/bank.html"
 
     def get_context_data(self, **kwargs):
         context = super(BankView, self).get_context_data(**kwargs)
