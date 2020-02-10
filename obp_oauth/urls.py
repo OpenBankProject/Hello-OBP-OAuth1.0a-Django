faa50774bd48a66f7c73434fdaa1503c7c9bfe0e
Peter Rosemann <dkdndes@gmail.com>
2020-02-10 17:34:04 +0100

    updated Django 3.0 default urls settings, dropped 1.7

--- obp_oauth/urls.py
+++ obp_oauth/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.urls import path
 
-from .views import IndexView, AuthorizationView, BankView
+from obp_oauth.views import IndexView, AuthorizationView, BankView
 
-urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^authorize/$', AuthorizationView.as_view(), name='authorize'),
-    url(r'^bank/$', BankView.as_view(), name='bank'),
-)
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('authorize/', AuthorizationView.as_view(), name='authorize'),
+    path('bank/', BankView.as_view(), name='bank'),
+    ]
